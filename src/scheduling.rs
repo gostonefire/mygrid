b2b100be4d702ae1a4749937aec5d19eb68339a8
gostonefire <peter.steneld@gmail.com>
2025-03-02 09:22:01 +0100

    adds tidy up

--- src/scheduling.rs
+++ src/scheduling.rs
@@ -276,15 +276,6 @@ impl Schedule {
     /// * 'production' - struct containing estimated hourly production levels
     /// * 'consumption' - struct containing estimated hourly load levels
     fn get_charge_level(selected_hours: Vec<u8>, production: &PVProduction, consumption: &Consumption) -> u8 {
-        /*
-        let segment = production.get_production()
-            .iter().enumerate()
-            .filter(|(h, _)| selected_hours.contains(&(*h as u8)))
-            .map(|(h, &p)|
-                Self::calculate_spare_capacity(p, consumption.get_consumption(h), consumption.get_min_avg_load())
-            )
-            .sum::<f64>() / selected_hours.len() as f64;
-        */
         let segment = production.get_production()
             .iter().enumerate()
             .filter(|(h, _)| selected_hours.contains(&(*h as u8)))
@@ -301,7 +292,6 @@ impl Schedule {
             charge_level = (100.0 - (segment.1 / segment.0 as f64) / SOC_CAPACITY_W).floor() as u8
         }
 
-        // let charge_level = (100.0 - segment / SOC_CAPACITY_W).floor() as u8;
         if charge_level != 100 {
             charge_level.min(95)
         } else {
