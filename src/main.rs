223d1f65efd7cf005f0912a5212d111de0b48a7c
gostonefire <peter.steneld@gmail.com>
2025-03-05 10:52:37 +0100

    fixes bug and finalize function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::Duration;
 use chrono::{DateTime, Local};
 use crate::initialization::init;
 use crate::worker::run;
@@ -43,15 +45,32 @@ fn main() {
     }
 }
 
+/// Manage top level errors
+///
+/// It prints the error to standard err and determines if we have had to many errors and thereby
+/// need to panic out of the application.
+///
+/// Errors are counted as long as they occur at least within an hour from each other, otherwise
+/// the counter is reset to last occurred error.
+///
+/// The function sleeps 10 minutes before releasing for a new try
+///
+/// # Arguments
+///
+/// * 'msg' - the error message to print to std err
+/// * 'n_errors' - the number of errors occurred so far with spacing under one hour
+/// * 'last_error' - the time the last error occurred
 fn manage_error(msg: String, mut n_errors: i32, last_error: DateTime<Local>) -> (i32, DateTime<Local>) {
     eprintln!("{}", msg);
 
-    if Local::now() - last_error > chrono::Duration::minutes(60) {
+    let now = Local::now();
+    if now - last_error > chrono::Duration::minutes(60) {
         n_errors = 1;
     } else if n_errors >= 10 {
         panic!();
     }
     n_errors += 1;
 
+    thread::sleep(Duration::from_secs(600));
     (n_errors, Local::now())
 }
