fd46f768bb3d143e7b9ea778b583fffe48e44573
gostonefire <peter.steneld@gmail.com>
2025-04-12 12:16:27 +0200

    adds better consumption curve given new heater system

--- src/consumption.rs
+++ src/consumption.rs
@@ -4,7 +4,7 @@ use crate::models::smhi_forecast::TimeValues;
 const MIN_AVG_LOAD: f64 = 200.0;
 
 /// Max average consumption/load in watts over an hour
-const MAX_AVG_LOAD: f64 = 3000.0;
+const MAX_AVG_LOAD: f64 = 2500.0;
 
 /// Struct for calculating and holding Consumption load per hour given a whether forecast
 ///
@@ -46,13 +46,31 @@ impl Consumption {
         let mut hour_load: [f64;24] = [0.0;24];
 
         for (h, v) in forecast.iter().enumerate() {
-            let temp = v.temp.max(0.0).min(20.0);
-            let load_factor = (20.0 - temp).powi(5) / 20.0_f64.powi(5);
-            let load = load_factor * (MAX_AVG_LOAD - MIN_AVG_LOAD) + MIN_AVG_LOAD;
-            hour_load[h] = load + day_consumption[h];
+            hour_load[h] = Consumption::consumption_curve(v.temp) + day_consumption[h];
         }
 
         self.hours = hour_load;
+    }
+
+    /// Calculates consumption based on temperature over a multiplicative inverse (1/x) curve.
+    /// The curve is formed such that it gives an approximation for a house consumption between
+    /// outside temperatures from -4 to 20. It is assumed that temperatures outside that range
+    /// doesn't change much on the consumption in the climate of southern Sweden.
+    ///
+    /// The factor is calculated such that the curve function is equal to 1 at X = -4 and 0 (zero)
+    /// at X = 20.
+    ///
+    /// Output thus varies between MAX_AVG_LOAD and MIN_AVG_LOAD
+    ///
+    /// # Arguments
+    ///
+    /// * 'temp' - outside temperature
+    fn consumption_curve(temp: f64) -> f64 {
+        let capped_temp = temp.max(-4.0).min(20.0);
+        let factor = 8.0 * 3.0f64.sqrt() - 8.0;
+        let curve = 2.0 / (capped_temp + factor) - 2.0 / ( 20.0 + factor);
+
+        curve * (MAX_AVG_LOAD - MIN_AVG_LOAD) + MIN_AVG_LOAD
     }
 }
 
